1cfbfa7546ed2ae1139b01bd5a44de5cf00cdc97
Sanket Kailkar <sanketkailkar.14@gmail.com>
2023-06-29 12:04:36 +0530

    Initial Commit

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='text-8xl'>
+      Hey Cool Dude 
+      Welcome to NEXT PWA
+    </main>
+  )
+}
